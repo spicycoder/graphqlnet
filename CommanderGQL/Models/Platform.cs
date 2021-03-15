0a009f110b5ba0e31ecf9a3008369f11fffc8126
spicycoder <spk2naveen@hotmail.com>
2021-03-15 22:18:40 +0530

    Documentation

--- CommanderGQL/Models/Platform.cs
+++ CommanderGQL/Models/Platform.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using HotChocolate;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace CommanderGQL.Models
 {
+    [GraphQLDescription("Represents any software or service, that has a command line interface")]
     public class Platform
     {
         [Key]
@@ -11,6 +13,7 @@ namespace CommanderGQL.Models
         [Required]
         public string Name { get; set; }
 
+        [GraphQLDescription("Represents a valid license for the platform")]
         public string LicenseKey { get; set; }
 
         public ICollection<Command> Commands { get; set; }
